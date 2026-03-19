ec9a1737136489867986a37cf3e964c7d6b5d44f
VuongLDT <thanhvuong92@gmail.com>
2026-03-19 21:58:11 +0700

    comment v4

--- SampleProject/Cmd_Lenhso2 .cs
+++ SampleProject/Cmd_Lenhso2 .cs
@@ -82,8 +82,8 @@ namespace Minhdh
                     string revSheet = doiTuong.LookupParameter(para_RevSheet).AsValueString();
 
                     // vuongldt: code này phải được đưa vào try/catch để tránh lỗi có chứa kí tự chữ sẽ không convert được sang int
-                    //zzz
-                    //
+                    // zzz
+                    // note
                     // Chuyển giá trị "RevSheet" từ string sang int
                     int revSheet_int = Convert.ToInt32(revSheet);
 
